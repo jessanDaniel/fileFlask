cf9b6578041ae5b097014f3791e797043b385d1f
jessanDaniel <jessandaniel@gmail.com>
2022-09-25 19:19:04 +0530

    third

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.arima.model import ARIMA
 import statsmodels.api as sm
+import json
 
 from werkzeug.utils import secure_filename
 import os
@@ -17,8 +18,9 @@ CORS(app)
 app.config['UPLOAD_FOLDER'] = './static'
 
 
-df=pd.DataFrame()
-chart_array=[]
+df = pd.DataFrame()
+chart_array = []
+
 
 @app.route('/')
 def welcome():
@@ -30,34 +32,37 @@ def fileUpload():
 
     file = request.files.get('file')
     global df
-    #df=pd.read_csv(file,parse_dates=True)
+    # df=pd.read_csv(file,parse_dates=True)
     filename = secure_filename(file.filename)
-    
+
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
     msg = "success"
     return jsonify({"response": msg})
 
+
 @app.route('/train', methods=['GET'])
 def train_data():
-    df=pd.read_csv("./static/jagane.csv",parse_dates=True)
-    #df.columns=["Month","Sales"]
-    #df.plot()
+    df = pd.read_csv("./static/jagane.csv", parse_dates=True)
+    # df.columns=["Month","Sales"]
+    # df.plot()
     df['Sales First Difference'] = df['Sales'] - df['Sales'].shift(12)
-    model=ARIMA(df['Sales'],order=(1,1,1))
-    model_fit=model.fit()
-    df['forecast']=model_fit.predict(start=90,end=103,dynamic=True)
-    #df[['Sales','forecast']].plot(figsize=(12,8))
-    model=sm.tsa.statespace.SARIMAX(df['Sales'],order=(1, 1, 1),seasonal_order=(1,1,1,12))
-    results=model.fit()
-    df['forecast']=results.predict(start=90,end=103,dynamic=True)
-    #df[['Sales','forecast']].plot(figsize=(12,8))
-    chart_array.append(df['forecast'][50:103])
-    resp={"chart":chart_array}
-    
-    print(chart_array)
-    return resp
-    
-    
+    model = ARIMA(df['Sales'], order=(1, 1, 1))
+    model_fit = model.fit()
+    df['forecast'] = model_fit.predict(start=90, end=103, dynamic=True)
+    # df[['Sales','forecast']].plot(figsize=(12,8))
+    model = sm.tsa.statespace.SARIMAX(
+        df['Sales'], order=(1, 1, 1), seasonal_order=(1, 1, 1, 12))
+    results = model.fit()
+    df['forecast'] = results.predict(start=90, end=103, dynamic=True)
+
+    # df = df.set_index(['Date'])
+    # df[['Sales','forecast']].plot(figsize=(12,8))
+    # chart_array.append(df['forecast'][50:103])
+
+    json_response = df['forecast'][50:103].to_json()
+    dates = df['Date'][50:103].to_json()
+    return {'sales': json_response, 'dates': dates}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
